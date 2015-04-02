1ba2f97f940d1b169b454df552f320aea4d487c4
sdtsui <danielt213@gmail.com>
2015-04-01 19:06:14 -0700

    best practices: largest5

--- codewars/largest-5-digit-number-in-a-series/largest5.js
+++ codewars/largest-5-digit-number-in-a-series/largest5.js
@@ -1,25 +1,19 @@
 //Lesson: clarify questions:
 ////'5 consecutive digits' does not necessarily refer to sum. It can refer to a number comprised of the string, from the 5 consecutive digits.
 
+
+//recursive solution...
 function solution(digits){
-  var allDigits = digits.split("");
-  for (var h = 0; h < digits.length; h++){
-    allDigits[h] = parseInt(allDigits[h]);
-  }
-  var numbers = {};
-  for (var i = 0 ; i < allDigits.length-4; i++){
-    var newNumber = parseInt(allDigits.slice(i, i+5).join(""));
-    numbers[i] = newNumber;
-  }
-  var max = numbers[0];
-  for (var idx in numbers){
-    if (numbers[idx] > max){
-      max = numbers[idx];
-    }
-  }
-  // console.log(typeof max);
-  // console.log(typeof max.toString());
-  // console.log(typeof parseInt(max));
-  console.log(typeof parseInt(max.toString()));
-  return parseFloat(max.toString());
+  if (digits.length <= 5) return Number(digits);
+  debugger;
+  return Math.max(Number(digits.substr(0, 5)), solution(digits.substr(1)));
 }
+
+//takeaways:
+//  substr: single argument: slices from 0 or that arg location, number
+//    two args: slices from 0 to 1.
+//    can replace substrings with new strings, by re-concatenating from indicies
+
+//Math.Math can be used to search comma separated number arguments
+//Math.max.apply(null, arr) can find the max in an array.
+//use of Number constructor instead of ParseInt
