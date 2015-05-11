6da9c8f2edb84e5ac10a3549ed2be51f1f5e655a
sdtsui <danielt213@gmail.com>
2015-05-11 14:01:33 -0700

    Completed: Grid Blast

---
+++ codewars/7thKyu/gridBlast/gridBlast.js
@@ -0,0 +1,8 @@
+//              THE BATTLEFIELD GRID
+// ['top left',    'top middle',    'top right',
+//  'middle left', 'center',        'middle right',
+//  'bottom left', 'bottom middle', 'bottom right']
+//
+function fire(x,y) {
+  console.log(grid);
+}
