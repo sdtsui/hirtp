ee737d98fea40bf4e426224eabb9088e3405b48d
sdtsui <danielt213@gmail.com>
2015-05-13 15:32:44 -0700

    Fix: Balancedparens with more functional style

--- codewars/6thKyu/checkingGroups.js
+++ codewars/6thKyu/checkingGroups.js
@@ -1,34 +1,29 @@
-//balancedParens
-
 function groupCheck(s){
-  //start with queue
-  //start adding to it:
-    //if IN and top match, nix
-    //if IN and bottom match, nix/delete bottom
-  //if any remaining elements, return false
-  var queue = [];
-  var input = s.split('');
-  console.log('input :', input);
-  var index = {
-    '(' : ['left', ')'],
-    '[' : ['left', ']'],
-    '{' : ['left', '}'],
-    ')' : ['right', '('],
-    ']' : ['right', '['],
-    '}' : ['right', '{']
+  var rightToLeft = function(s){
+    return (
+      (s === ')') ? '(' :
+      (s === ']') ? '[' :
+      (s === '}') ? '{' : 
+      undefined
+      )
   };
-  input.forEach(function(el, idx){
-    if (index[el][0] === 'right'){
-      if (queue[queue.length-1] === index[el][1]) {
+  var isLeft = function(s) {
+    return (
+      ((s === '(' || s === '[') || s === '{')
+      )
+  };
+  var queue = [];
+  s.split('').forEach(function(el, idx){
+    if (isLeft(s)){
+      queue.push(el);
+    } else {
+      if (queue[queue.length-1] === rightToLeft(el)){
         queue.pop();
       } else {
         queue.push(el);
-      }      
-    } else {
-      queue.push(el);
-    } 
-    console.log('queue mid: ', queue);
+      }
+    }
   });
-  console.log('queue End: ', queue);
+  
   return (queue.length === 0) ? true : false;
 }
